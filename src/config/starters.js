066a8fc1390a693615941b2bb19e6136eb8f1e4f
David Kolosza <david.kolosza@gmx.de>
2026-03-24 16:59:18 +0100

    picture path update

--- src/config/starters.js
+++ src/config/starters.js
@@ -7,7 +7,7 @@ module.exports = [
     style: 'Natur / Nahrung / Support',
     ability: 'Grüner Daumen – bessere Nahrungsausbeute und kleine Heilung nach Arbeit.',
     stats: { kraft: 2, tempo: 2, ausdauer: 3, instinkt: 3, geschick: 4 },
-    imageUrl: `${IMAGE_BASE_URL}/bulbasaur.png`
+    imageUrl: `https://cdn.discordapp.com/attachments/1486030100641222706/1486030254802731208/bulbasaur.png?ex=69c40505&is=69c2b385&hm=201d7350543bde2387106a40ac2136a59418ed557232fd555a60f98163841505&`
   },
   {
     key: 'charmander',
@@ -16,7 +16,7 @@ module.exports = [
     style: 'Schmiede / Tempo / Offensive',
     ability: 'Heißblütig – schneller bei Werkstatt- und Kampfarbeit.',
     stats: { kraft: 3, tempo: 4, ausdauer: 2, instinkt: 2, geschick: 3 },
-    imageUrl: `${IMAGE_BASE_URL}/charmander.png`
+    imageUrl: `https://cdn.discordapp.com/attachments/1486030100641222706/1486030255578812539/charmander.png?ex=69c40506&is=69c2b386&hm=04252b03aaec6561420d0dddabae6cea5058f1a27dadde74822d2bf7ae10d256&`
   },
   {
     key: 'squirtle',
@@ -25,7 +25,7 @@ module.exports = [
     style: 'Bau / Verteidigung / Zuverlässigkeit',
     ability: 'Standhaft – bessere Beiträge beim Ausbau und Schutz des Lagers.',
     stats: { kraft: 3, tempo: 2, ausdauer: 5, instinkt: 2, geschick: 3 },
-    imageUrl: `${IMAGE_BASE_URL}/squirtle.png`
+    imageUrl: `https://cdn.discordapp.com/attachments/1486030100641222706/1486030281013198981/squirtle.png?ex=69c4050c&is=69c2b38c&hm=0b69b08ab8e9de1a6aaf4ce52835f019d85a833e9b8c2d7ac306354aada76377&`
   },
   {
     key: 'pikachu',
@@ -34,7 +34,7 @@ module.exports = [
     style: 'Energie / Technik / Geschwindigkeit',
     ability: 'Stromstoß – kürzere Abklingzeiten bei vielen Aktionen.',
     stats: { kraft: 2, tempo: 5, ausdauer: 2, instinkt: 3, geschick: 4 },
-    imageUrl: `${IMAGE_BASE_URL}/pikachu.png`
+    imageUrl: `https://cdn.discordapp.com/attachments/1486030100641222706/1486030253531861043/pikachu.png?ex=69c40505&is=69c2b385&hm=806bd9ec9f94ebeddf3c6a4ee1205fa2b55a23c44c6959ae68523c88bcb9cdd2&`
   },
   {
     key: 'eevee',
@@ -43,7 +43,7 @@ module.exports = [
     style: 'Allround / Anpassung / Zukunft',
     ability: 'Anpassungsfähig – kleiner Bonus auf fast alle Standardaktionen.',
     stats: { kraft: 3, tempo: 3, ausdauer: 3, instinkt: 3, geschick: 3 },
-    imageUrl: `${IMAGE_BASE_URL}/eevee.png`
+    imageUrl: `https://cdn.discordapp.com/attachments/1486030100641222706/1486030256719663164/eevee.png?ex=69c40506&is=69c2b386&hm=241b0b8a163b1c25727e8d598594599e33e622d6d3491fb98f65245a1bf9a064&`
   },
   {
     key: 'abra',
@@ -52,7 +52,7 @@ module.exports = [
     style: 'Forschung / Effizienz / seltene Funde',
     ability: 'Mentale Klarheit – höhere Chance auf besondere Entdeckungen.',
     stats: { kraft: 1, tempo: 3, ausdauer: 2, instinkt: 4, geschick: 5 },
-    imageUrl: `${IMAGE_BASE_URL}/abra.png`
+    imageUrl: `https://cdn.discordapp.com/attachments/1486030100641222706/1486030254098350111/abra.png?ex=69c40505&is=69c2b385&hm=2f1b45618fb338512d3eb6a27f683f225f6d9affcb86cf409dcb4c77ddbd0692&`
   },
   {
     key: 'gastly',
@@ -61,7 +61,7 @@ module.exports = [
     style: 'Nacht / Ruinen / Mystik',
     ability: 'Zwielichtjäger – stärker bei Nacht und seltenen Ereignissen.',
     stats: { kraft: 2, tempo: 4, ausdauer: 2, instinkt: 5, geschick: 3 },
-    imageUrl: `${IMAGE_BASE_URL}/gastly.png`
+    imageUrl: `https://cdn.discordapp.com/attachments/1486030100641222706/1486030257990664254/gastly.png?ex=69c40506&is=69c2b386&hm=93b443fbd7e9e1194112b522797eb153ab9e053cce0cdc4050274855c32174fc&`
   },
   {
     key: 'dratini',
@@ -70,7 +70,7 @@ module.exports = [
     style: 'Lategame / Wachstum / Potenzial',
     ability: 'Verborgene Kraft – langsam am Anfang, stark im späteren Verlauf.',
     stats: { kraft: 2, tempo: 2, ausdauer: 4, instinkt: 3, geschick: 4 },
-    imageUrl: `${IMAGE_BASE_URL}/dratini.png`
+    imageUrl: `https://cdn.discordapp.com/attachments/1486030100641222706/1486030256245575770/dratini.png?ex=69c40506&is=69c2b386&hm=a25fef28027b48ba1f5fd9f92ca8d1c67fc833cf84c79911f459ef35acc7ad17&`
   },
   {
     key: 'lucario',
@@ -79,7 +79,7 @@ module.exports = [
     style: 'Elite / Kampf / Aura',
     ability: 'Aurafokus – besonders stark bei Kampf, Wache und Spezialaufgaben.',
     stats: { kraft: 4, tempo: 4, ausdauer: 3, instinkt: 4, geschick: 3 },
-    imageUrl: `${IMAGE_BASE_URL}/lucario.png`
+    imageUrl: `https://cdn.discordapp.com/attachments/1486030100641222706/1486030258493853856/lucario.png?ex=69c40506&is=69c2b386&hm=cf87102bb4b0eed3ba23ad05c30ae63fdb78417b6eeead89964c48d199a966b6&`
   },
   {
     key: 'tinkaton',
@@ -88,7 +88,7 @@ module.exports = [
     style: 'Werkbank / Schmiede / Material',
     ability: 'Meisterhammer – effizient bei Bau, Werkstatt und Materialverarbeitung.',
     stats: { kraft: 4, tempo: 2, ausdauer: 3, instinkt: 2, geschick: 4 },
-    imageUrl: `${IMAGE_BASE_URL}/tinkaton.png`
+    imageUrl: `https://cdn.discordapp.com/attachments/1486030100641222706/1486030281503936512/tinkaton.png?ex=69c4050c&is=69c2b38c&hm=a5d6f5389910c15fe5f8852e3605f471571526ef951a81abab9d68fc08939dac&`
   },
   {
     key: 'treecko',
@@ -97,7 +97,7 @@ module.exports = [
     style: 'Erkundung / Wald / Tempo',
     ability: 'Waldläufer – schneller beim Scouting und Sammeln im Grünen.',
     stats: { kraft: 2, tempo: 5, ausdauer: 2, instinkt: 4, geschick: 3 },
-    imageUrl: `${IMAGE_BASE_URL}/treecko.png`
+    imageUrl: `https://cdn.discordapp.com/attachments/1486030100641222706/1486030282405711902/treecko.png?ex=69c4050c&is=69c2b38c&hm=382b5ce8026561f9c177fc53fda193f7a96bd1cf1bf651d3858985c822637ee1&`
   },
   {
     key: 'mareep',
@@ -106,7 +106,7 @@ module.exports = [
     style: 'Energie / Infrastruktur / Technik',
     ability: 'Flauschige Leitung – Boni auf Technik, Energie und spätere Infrastruktur.',
     stats: { kraft: 2, tempo: 2, ausdauer: 4, instinkt: 3, geschick: 4 },
-    imageUrl: `${IMAGE_BASE_URL}/mareep.png`
+    imageUrl: `https://cdn.discordapp.com/attachments/1486030100641222706/1486030259257344202/mareep.png?ex=69c40506&is=69c2b386&hm=d2b4edc443a7b8c2bd0b6aa40d3d554504d92a1e3697e3a8052078bd59008def&`
   },
   {
     key: 'rowlet',
@@ -115,7 +115,7 @@ module.exports = [
     style: 'Wald / Ruhe / Präzision',
     ability: 'Schattenfeder – stark beim Erkunden, Jagen und vorsichtigen Sammeln.',
     stats: { kraft: 2, tempo: 3, ausdauer: 3, instinkt: 4, geschick: 4 },
-    imageUrl: `${IMAGE_BASE_URL}/rowlet.png`
+    imageUrl: `https://cdn.discordapp.com/attachments/1486030100641222706/1486030280560218193/rowlet.png?ex=69c4050b&is=69c2b38b&hm=19f5e480e8259cad9f3295db90f9ef29654d731836c1632dd379f850a78eb20e&`
   },
   {
     key: 'totodile',
@@ -124,7 +124,7 @@ module.exports = [
     style: 'Wasser / Wildheit / Ausdauer',
     ability: 'Bissiger Eifer – stark bei robusten Einsätzen und Wasserprojekten.',
     stats: { kraft: 4, tempo: 3, ausdauer: 4, instinkt: 2, geschick: 2 },
-    imageUrl: `${IMAGE_BASE_URL}/totodile.png`
+    imageUrl: `https://cdn.discordapp.com/attachments/1486030100641222706/1486030281889546471/totodile.png?ex=69c4050c&is=69c2b38c&hm=759dcec9fc8778088c615203b5831017376a7e7532a450a93d217346fec49750&`
   },
   {
     key: 'fuecoco',
@@ -133,6 +133,6 @@ module.exports = [
     style: 'Feuer / Gemüt / Durchhaltevermögen',
     ability: 'Warmer Magen – gut bei Versorgung, Feuerstellen und langen Arbeiten.',
     stats: { kraft: 3, tempo: 2, ausdauer: 5, instinkt: 2, geschick: 2 },
-    imageUrl: `${IMAGE_BASE_URL}/fuecoco.png`
+    imageUrl: `https://cdn.discordapp.com/attachments/1486030100641222706/1486030257457725532/fuecoco.png?ex=69c40506&is=69c2b386&hm=a5206d738cbc856b356d5c3d7fb05dbc14b1ffed29fd41a874bd6d84799ed720&`
   }
 ];
