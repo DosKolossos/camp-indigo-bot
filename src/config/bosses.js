69b28161d59d6412ec6a913d4095cf3da310426f
David Kolosza <david.kolosza@gmx.de>
2026-04-04 12:40:44 +0200

    boss update

--- src/config/bosses.js
+++ src/config/bosses.js
@@ -3,15 +3,15 @@ module.exports = [
     key: 'charizard',
     name: 'Glurak',
     emoji: '🔥',
-    bossPower: 68,
+    bossPower: 100,
     intro: 'Ein wildes Glurak kreist über dem Camp und sucht die Konfrontation.',
     rewards: {
       win: {
         xp: [14, 20],
-        food: [2, 4],
-        stone: [0, 2],
-        ore: [1, 2],
-        scrap: [1, 2]
+        food: [5, 7],
+        stone: [1, 3],
+        ore: [3, 4],
+        scrap: [2, 3]
       },
       lose: {
         xp: [5, 8]
@@ -22,15 +22,15 @@ module.exports = [
     key: 'mewtwo',
     name: 'Mewtu',
     emoji: '🧠',
-    bossPower: 82,
+    bossPower: 120,
     intro: 'Mewtu wurde vom Lager angelockt und testet die Stärke des Camps.',
     rewards: {
       win: {
         xp: [18, 24],
-        food: [1, 3],
-        ore: [1, 3],
-        fiber: [1, 2],
-        scrap: [1, 3]
+        food: [5, 9],
+        ore: [3, 4],
+        fiber: [2, 4],
+        scrap: [2, 3]
       },
       lose: {
         xp: [6, 9]
@@ -41,14 +41,14 @@ module.exports = [
     key: 'kyogre',
     name: 'Kyogre',
     emoji: '🌊',
-    bossPower: 76,
+    bossPower: 105,
     intro: 'Kyogre taucht aus der Tiefe auf und bedroht die Vorräte des Camps.',
     rewards: {
       win: {
         xp: [16, 22],
-        food: [3, 5],
-        stone: [1, 2],
-        ore: [1, 2]
+        food: [4, 8],
+        stone: [2, 3],
+        ore: [3, 4]
       },
       lose: {
         xp: [5, 8]
@@ -59,14 +59,14 @@ module.exports = [
     key: 'tyranitar',
     name: 'Despotar',
     emoji: '🪨',
-    bossPower: 72,
+    bossPower: 103,
     intro: 'Ein Despotar wütet in der Nähe und muss gemeinsam gestoppt werden.',
     rewards: {
       win: {
         xp: [15, 21],
         food: [1, 3],
-        stone: [2, 4],
-        ore: [1, 2]
+        stone: [5, 10],
+        ore: [2, 4]
       },
       lose: {
         xp: [5, 8]
@@ -77,12 +77,12 @@ module.exports = [
     key: 'dragonite',
     name: 'Dragoran',
     emoji: '🐉',
-    bossPower: 74,
+    bossPower: 106,
     intro: 'Ein mächtiges Dragoran wurde auf das Camp aufmerksam.',
     rewards: {
       win: {
         xp: [16, 22],
-        food: [2, 4],
+        food: [5, 7],
         fiber: [1, 3],
         scrap: [1, 2]
       },
@@ -95,15 +95,15 @@ module.exports = [
     key: 'zapdos',
     name: 'Zapdos',
     emoji: '⚡',
-    bossPower: 78,
+    bossPower: 104,
     intro: 'Zapdos entlädt seine Energie über dem Camp.',
     rewards: {
       win: {
         xp: [17, 23],
-        food: [1, 3],
-        ore: [1, 2],
-        fiber: [1, 2],
-        scrap: [2, 3]
+        food: [2, 5],
+        ore: [3, 4],
+        fiber: [3, 4],
+        scrap: [4, 6]
       },
       lose: {
         xp: [6, 9]
